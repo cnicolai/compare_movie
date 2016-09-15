168152fcba2192f5470819de1464cc245f6ea766
Christoph Nicolai <mail@christoph-nicolai.de>
2016-09-15 16:12:47 +0200

    Removed random start

--- compare_movie.sh
+++ compare_movie.sh
@@ -2,18 +2,15 @@
 
 n=$((RANDOM%2+1))
 
-#assume video length is at least fifteen minutes
-start=$((RANDOM%860+120))
-
 if test $n = 1
 then
-	mplayer -ss $start -endpos 30 -really-quiet -nosound -geometry 1280x1417+0+0 -vf crop=1280:1417:0:0 $1 &
-	mplayer -ss $start -endpos 30 -really-quiet -nosound -geometry 1280x1417+1417+0 -vf crop=1280:1417:0:0 $2 &
+	mplayer -really-quiet -nosound -geometry 1280x1417+0+0 -vf crop=1280:1417:0:0 $1 &
+	mplayer -really-quiet -nosound -geometry 1280x1417+1417+0 -vf crop=1280:1417:0:0 $2 &
 	sleep 32
 	echo "Left: $1, Right: $2"
 else
-	mplayer -ss $start -endpos 30 -really-quiet -nosound -geometry 1280x1417+0+0 -vf crop=1280:1417:0:0 $2 &
-	mplayer -ss $start -endpos 30 -really-quiet -nosound -geometry 1280x1417+1417+0 -vf crop=1280:1417:0:0 $1 &
+	mplayer -really-quiet -nosound -geometry 1280x1417+0+0 -vf crop=1280:1417:0:0 $2 &
+	mplayer -really-quiet -nosound -geometry 1280x1417+1417+0 -vf crop=1280:1417:0:0 $1 &
 	sleep 32
 	echo "Left: $2, Right: $1"
 fi
