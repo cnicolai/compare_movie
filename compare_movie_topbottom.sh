9f3c3e6c9cd6b7b66096acd6615323bd78c8a364
Christoph Nicolai <mail@christoph-nicolai.de>
2017-10-19 11:59:49 +0200

    Update scripts

--- compare_movie_topbottom.sh
+++ compare_movie_topbottom.sh
@@ -6,15 +6,15 @@ if test $n = 1
 then
 	# mplayer -ss 0 -endpos 30 -really-quiet -nosound -geometry 1280x1417+0+0 -vf crop=1280:1417:0:0 $1
 	# mplayer -ss 0 -endpos 30 -really-quiet -nosound -geometry 1280x1417+1417+0 -vf crop=1280:1417:0:0 $2
-	mpv --end=30 --no-audio $1 --title=links  --geometry=1280x1440+0+0 &
-	mpv --end=30 --no-audio $2 --title=rechts --geometry=1280x1440+1280+0 &
+	mpv --end=30 --no-audio $1 --title=oben  --geometry=2560x720+0+0 &
+	mpv --end=30 --no-audio $2 --title=unten --geometry=2560x720+0+720 &
 	sleep 31
-	echo "Left: $1, Right: $2"
+	echo "Oben: $1, Unten: $2"
 else
 	# mplayer -ss 0 -endpos 30 -really-quiet -nosound -geometry 1280x1417+0+0 -vf crop=1280:1417:0:0 $2
 	# mplayer -ss 0 -endpos 30 -really-quiet -nosound -geometry 1280x1417+1417+0 -vf crop=1280:1417:0:0 $1
-	mpv --end=30 --no-audio $2 --title=links  --geometry=1280x1440+0+0 &
-	mpv --end=30 --no-audio $1 --title=rechts --geometry=1280x1440+1280+0 &
+	mpv --end=30 --no-audio $2 --title=oben  --geometry=2560x720+0+0 &
+	mpv --end=30 --no-audio $1 --title=unten --geometry=2560x720+0+720 &
 	sleep 31
-	echo "Left: $2, Right: $1"
+	echo "Oben: $2, Unten: $1"
 fi
